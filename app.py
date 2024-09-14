21062846d27d4ea1aaf42648be9ce22aed268b8a
NandhiniRavi01 <113678413+NandhiniRavi01@users.noreply.github.com>
2024-09-14 13:53:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World! My first' 
+    return 'Hello, World! My sample web page' 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5006)
