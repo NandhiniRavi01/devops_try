b589399be27c383bc860a4e3856e393f2ee731ae
NandhiniRavi01 <113678413+NandhiniRavi01@users.noreply.github.com>
2024-09-14 13:24:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-'''
+
 @app.route('/')
 def home():
-    return 'Hello, World!' '''
-
-@app.route('/github',method=[''POST])
-def api_ngrok():
-    if request.headers['Content-Type'] =='application/json'
-       request json.dumps(request.json)
+    return 'Hello, World!' 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5006)
