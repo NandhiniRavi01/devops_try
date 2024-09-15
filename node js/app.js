38c16a9a39380e6fef3744a3cdfe0097318bb5a6
NandhiniRavi01 <113678413+NandhiniRavi01@users.noreply.github.com>
2024-09-15 19:59:38 +0530

    Update app.js

--- node js/app.js
+++ node js/app.js
@@ -1,18 +1,11 @@
-// Import the HTTP module
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// Define the hostname and port where the server will listen
-const hostname = '127.0.0.1'; // Localhost
-const port = 3000;            // Port number
-
-// Create an HTTP server that sends a "Hello, World!" response
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;               // Set status code to 200 (OK)
-  res.setHeader('Content-Type', 'text/plain'); // Set response type
-  res.end('Hello, World! , This is my first demo');         // End the response with a message
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-// Make the server listen on the specified hostname and port
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`); // Log the server URL
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App running at http://0.0.0.0:${port}`);
 });
