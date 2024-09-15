947d34f1f7051408084d234456ccc63901fb61df
NandhiniRavi01 <113678413+NandhiniRavi01@users.noreply.github.com>
2024-09-15 20:17:20 +0530

    Create index.js

---
+++ node js/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!, My first sample');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App running at http://0.0.0.0:${port}`);
+});
