1560b8dfe8b30ebc88705583b59a763ec8bf6c19
NandhiniRavi01 <113678413+NandhiniRavi01@users.noreply.github.com>
2024-09-17 18:46:09 +0530

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3002;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!, My first sample');
+  res.send('Hello World!, My second sample');
 });
 
 app.listen(port, '0.0.0.0', () => {
